05dadf6a3dcdf85a47144d464225481167db7e68
riverb00k <river.book.kotoe@gmail.com>
2022-12-13 14:19:56 +0900

    メール認証ができるようになりました。

--- lib/view/start_up/check_email_page.dart
+++ lib/view/start_up/check_email_page.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:oshi_map/utils/authentication.dart';
+import 'package:oshi_map/utils/firestore/users.dart';
 import 'package:oshi_map/utils/widget_utils.dart';
+import 'package:oshi_map/view/screen.dart';
 
 class CheckEmailPage extends StatefulWidget {//stf
   //この画面に遷移するタイミングで、emilとpassの情報をもらう
@@ -20,7 +24,25 @@ class _CheckEmailPageState extends State<CheckEmailPage> {
       body: Column(
         children: [
           Text('登録いただいたメールアドレス宛てに確認のメールを送信しております。記載されているURLをクリックして認証を終わらせて下さい。'),
-
+          ElevatedButton(
+              onPressed: () async{
+                var result = await Authentication.emailSignIn(email: widget.email, pass: widget.pass);
+                //違うクラスで作られているので、widgetおつける
+                if(result is UserCredential) { //ログインが上手くいっているとき
+                  if (result.user!.emailVerified == true) {
+                    //email認証が済んでいるユーザーか？
+                    while (Navigator.canPop(context)) {
+                      Navigator.pop(context);
+                    }
+                    await UserFirestore.getUser(result.user!.uid);
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => Screen()));
+                  }else{//メール認証が終わってない
+                    print('メール認証が終わっていません。');
+                  }
+                }
+                },
+              child: Text('認証完了'))
         ],
       ),
 
